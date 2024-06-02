7443ed54fa7985ec9afec52b76233219ffbc5729
ShiraPliskin <shirap803@gmail.com>
2024-06-02 12:30:45 +0300

    use mui-dialog in add donor

--- Client/src/components/Donors/DonorAdd.jsx
+++ Client/src/components/Donors/DonorAdd.jsx
@@ -1,130 +1,117 @@
-import { useState, useEffect } from "react";
+// import { useState, useEffect } from "react";
+// import DonorForm from "./DonorForm";
+
+// const DonorAdd = (fields) => {
+
+//     const [donorDetails, setDonorDetails] = useState("");
+//     const [displayAddForm, setDisplayAddForm] = useState(false);
+//     const [commentArea, setCommentArea] = useState("");
+
+//     useEffect(() => {
+//         setDonorDetails(fields);
+//     }, [])
+
+//     const handleSubmit = (e) => {
+//         e.preventDefault();
+    
+//     };
+
+//     const handleChange = (e) => {
+//         const { name, value } = e.target;
+//         setDonorDetails((prevData) => ({ ...prevData, [name]: value }));
+//     }
+
+//     return(
+//         <>
+//         <button onClick={()=>setDisplayAddForm((prev) => !prev)}>הוספת תורם</button>
+//         {displayAddForm && (
+//             <form onSubmit={handleSubmit}>
+//             <label>
+//                 <input type="text" name="f_name" value={donorDetails.f_name} onChange={handleChange} placeholder="שם פרטי" noValidate />
+//             </label>
+//             <label>
+//                 <input type="text" name="l_name" value={donorDetails.l_name} onChange={handleChange} placeholder="שם משפחה" noValidate />
+//             </label>
+//             <label>
+//                     <input type="text" name="phone" value={donorDetails.phone} onChange={handleChange} placeholder="טלפון" noValidate />
+//             </label>
+//             <label>
+//                 <input type="email" name="email" value={donorDetails.email} onChange={handleChange} placeholder="כתובת מייל" noValidate />
+//             </label>
+//             <label>
+//                 <input type="text" name="address" value={donorDetails.address} onChange={handleChange} placeholder="כתובת" noValidate />
+//             </label>
+//             <label>
+//                 <input type="number" name="num_of_children" value={donorDetails.num_of_children} onChange={handleChange} placeholder="מספר ילדים" noValidate />
+//             </label>
+//             <label>
+//                 <input type="text" name="spouse_name" value={donorDetails.spouse_name} onChange={handleChange} placeholder="שם בן הזוג" noValidate />
+//             </label>
+//             <label>
+//                 <input type="text" name="address_at_work" value={donorDetails.address_at_work} onChange={handleChange} placeholder="כתובת בעבודה" noValidate />
+//             </label>
+//             <label>
+//                 <input type="text" name="introduction_description" value={donorDetails.introduction_description} onChange={handleChange} placeholder="תיאור הכרות" noValidate />
+//             </label>
+
+//             {/* contact_id */}
+//             <label>
+//                 <input type="text" name="remarks" value={donorDetails.remarks} onChange={handleChange} placeholder="הערות" noValidate />
+//             </label>
+
+//             <button type="submit">הוסף</button>
+
+//             {<p>{commentArea}</p>}
+//         </form>
+//         )}
+//         </>
+//     )
+// }
+// export default DonorAdd;
+
+import React, { useState, useEffect } from "react";
+import { Button } from "@mui/material";
 import DonorForm from "./DonorForm";
 
-const DonorAdd = (fields) => {
-
-    const [donorDetails, setDonorDetails] = useState("");
-    const [displayAddForm, setDisplayAddForm] = useState(false);
+const DonorAdd = ({ fields }) => {
+    const [donorDetails, setDonorDetails] = useState({});
+    const [open, setOpen] = useState(false);
     const [commentArea, setCommentArea] = useState("");
 
     useEffect(() => {
         setDonorDetails(fields);
-    }, [])
+    }, [fields]);
+
+    const handleClickOpen = () => {
+        setOpen(true);
+    };
+
+    const handleClose = () => {
+        setOpen(false);
+    };
 
     const handleSubmit = (e) => {
         e.preventDefault();
-    
+        // Add your submit logic here
+        setCommentArea("Donor added successfully");
+        handleClose();
     };
 
     const handleChange = (e) => {
         const { name, value } = e.target;
         setDonorDetails((prevData) => ({ ...prevData, [name]: value }));
-    }
+    };
 
-    return(
+    return (
         <>
-        <button onClick={()=>setDisplayAddForm((prev) => !prev)}>הוספת תורם</button>
-        {displayAddForm && (
-            <form onSubmit={handleSubmit}>
-            <label>
-                <input type="text" name="f_name" value={donorDetails.f_name} onChange={handleChange} placeholder="שם פרטי" noValidate />
-            </label>
-            <label>
-                <input type="text" name="l_name" value={donorDetails.l_name} onChange={handleChange} placeholder="שם משפחה" noValidate />
-            </label>
-            <label>
-                    <input type="text" name="phone" value={donorDetails.phone} onChange={handleChange} placeholder="טלפון" noValidate />
-            </label>
-            <label>
-                <input type="email" name="email" value={donorDetails.email} onChange={handleChange} placeholder="כתובת מייל" noValidate />
-            </label>
-            <label>
-                <input type="text" name="address" value={donorDetails.address} onChange={handleChange} placeholder="כתובת" noValidate />
-            </label>
-            <label>
-                <input type="number" name="num_of_children" value={donorDetails.num_of_children} onChange={handleChange} placeholder="מספר ילדים" noValidate />
-            </label>
-            <label>
-                <input type="text" name="spouse_name" value={donorDetails.spouse_name} onChange={handleChange} placeholder="שם בן הזוג" noValidate />
-            </label>
-            <label>
-                <input type="text" name="address_at_work" value={donorDetails.address_at_work} onChange={handleChange} placeholder="כתובת בעבודה" noValidate />
-            </label>
-            <label>
-                <input type="text" name="introduction_description" value={donorDetails.introduction_description} onChange={handleChange} placeholder="תיאור הכרות" noValidate />
-            </label>
-
-            {/* contact_id */}
-            <label>
-                <input type="text" name="remarks" value={donorDetails.remarks} onChange={handleChange} placeholder="הערות" noValidate />
-            </label>
-
-            <button type="submit">הוסף</button>
-
-            {<p>{commentArea}</p>}
-        </form>
-        )}
+            <Button variant="outlined" onClick={handleClickOpen}>
+                הוספת תורם
+            </Button>
+            <DonorForm donorDetails={donorDetails} handleChange={handleChange} handleSubmit={handleSubmit} open={open} handleClose={handleClose} />
+           
+            {commentArea && <p>{commentArea}</p>}
         </>
-    )
-}
+    );
+};
+
 export default DonorAdd;
-
-// import React, { useState } from "react";
-// import { createRoot } from 'react-dom/client';
-// import ReactDOM from 'react-dom/client';
-// import DonorForm from "./DonorForm";
-
-// const DonorAdd = (fields) => {
-//     const [commentArea, setCommentArea] = useState("");
-
-//     const handleSubmit = (newDonorDetails) => {
-//         console.log("Form submitted:", newDonorDetails);
-//         setCommentArea("Donor added successfully");
-//         window.close(); // Close the form window after submission
-//     };
-
-//     const openFormWindow = () => {
-//         const formWindow = window.open(
-//             "",
-//             "_blank",
-//             "width=600,height=600"
-//         );
-
-//         formWindow.document.write(`
-//             <!DOCTYPE html>
-//             <html lang="en">
-//             <head>
-//                 <meta charset="UTF-8">
-//                 <meta name="viewport" content="width=device-width, initial-scale=1.0">
-//                 <title>Donor Form</title>
-//             </head>
-//             <body>
-//                 <div id="form-root"></div>
-//             </body>
-//             </html>
-//         `);
-
-//         formWindow.document.close();
-
-//         // Ensure React and ReactDOM are available in the new window
-//         formWindow.React = React;
-//         formWindow.ReactDOM = ReactDOM;
-
-//         // Use createRoot instead of ReactDOM.render
-//         const root = formWindow.ReactDOM.createRoot(formWindow.document.getElementById("form-root"));
-//         root.render(
-//             <formWindow.React.StrictMode>
-//                 <DonorAddForm initialFields={fields} onSubmit={handleSubmit} />
-//             </formWindow.React.StrictMode>
-//         );
-//     };
-
-//     return (
-//         <>
-//             <button onClick={openFormWindow}>הוספת תורם</button>
-//             {<p>{commentArea}</p>}
-//         </>
-//     );
-// };
-
-// export default DonorAdd;
--- Client/src/components/Donors/DonorForm.jsx
+++ Client/src/components/Donors/DonorForm.jsx
@@ -1,52 +1,130 @@
 import { useState } from "react";
+import { Button, TextField, Dialog, DialogActions, DialogContent, DialogTitle, createTheme,ThemeProvider } from "@mui/material";
 
-const DonorForm = ({ initialFields, onSubmit }) => {
-    const [donorDetails, setDonorDetails] = useState(initialFields);
+const theme = createTheme({
+    components: {
+        MuiTextField: {
+            styleOverrides: {
+                root: {
+                    '& .MuiInputBase-input': {
+                        textAlign: "right",
+                    },
+                },
+            },
+        },
+    },
+});
 
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        onSubmit(donorDetails);
-    };
+const DonorForm = ({ donorDetails, handleChange, handleSubmit, open, handleClose }) => {
 
-    const handleChange = (e) => {
-        const { name, value } = e.target;
-        setDonorDetails((prevData) => ({ ...prevData, [name]: value }));
-    };
 
     return (
-        <form onSubmit={handleSubmit}>
-            <label>
-                <input type="text" name="f_name" value={donorDetails.f_name} onChange={handleChange} placeholder="שם פרטי" noValidate />
-            </label>
-            <label>
-                <input type="text" name="l_name" value={donorDetails.l_name} onChange={handleChange} placeholder="שם משפחה" noValidate />
-            </label>
-            <label>
-                <input type="text" name="phone" value={donorDetails.phone} onChange={handleChange} placeholder="טלפון" noValidate />
-            </label>
-            <label>
-                <input type="email" name="email" value={donorDetails.email} onChange={handleChange} placeholder="כתובת מייל" noValidate />
-            </label>
-            <label>
-                <input type="text" name="address" value={donorDetails.address} onChange={handleChange} placeholder="כתובת" noValidate />
-            </label>
-            <label>
-                <input type="number" name="num_of_children" value={donorDetails.num_of_children} onChange={handleChange} placeholder="מספר ילדים" noValidate />
-            </label>
-            <label>
-                <input type="text" name="spouse_name" value={donorDetails.spouse_name} onChange={handleChange} placeholder="שם בן הזוג" noValidate />
-            </label>
-            <label>
-                <input type="text" name="address_at_work" value={donorDetails.address_at_work} onChange={handleChange} placeholder="כתובת בעבודה" noValidate />
-            </label>
-            <label>
-                <input type="text" name="introduction_description" value={donorDetails.introduction_description} onChange={handleChange} placeholder="תיאור הכרות" noValidate />
-            </label>
-            <label>
-                <input type="text" name="remarks" value={donorDetails.remarks} onChange={handleChange} placeholder="הערות" noValidate />
-            </label>
-            <button type="submit">הוסף</button>
-        </form>
+        <>
+            <ThemeProvider theme={theme}>
+                <Dialog open={open} onClose={handleClose}>
+                    <DialogTitle>הוספת תורם</DialogTitle>
+                    <DialogContent>
+                        <form onSubmit={handleSubmit}>
+                            <TextField
+                                autoFocus
+                                margin="dense"
+                                name="f_name"
+                                label="שם פרטי"
+                                type="text"
+                                fullWidth
+                                value={donorDetails.f_name || ""}
+                                onChange={handleChange}
+                            />
+                            <TextField
+                                margin="dense"
+                                name="l_name"
+                                label="שם משפחה"
+                                type="text"
+                                fullWidth
+                                value={donorDetails.l_name || ""}
+                                onChange={handleChange}
+                            />
+                            <TextField
+                                margin="dense"
+                                name="phone"
+                                label="טלפון"
+                                type="text"
+                                fullWidth
+                                value={donorDetails.phone || ""}
+                                onChange={handleChange}
+                            />
+                            <TextField
+                                margin="dense"
+                                name="email"
+                                label="כתובת מייל"
+                                type="email"
+                                fullWidth
+                                value={donorDetails.email || ""}
+                                onChange={handleChange}
+                            />
+                            <TextField
+                                margin="dense"
+                                name="address"
+                                label="כתובת"
+                                type="text"
+                                fullWidth
+                                value={donorDetails.address || ""}
+                                onChange={handleChange}
+                            />
+                            <TextField
+                                margin="dense"
+                                name="num_of_children"
+                                label="מספר ילדים"
+                                type="number"
+                                fullWidth
+                                value={donorDetails.num_of_children || ""}
+                                onChange={handleChange}
+                            />
+                            <TextField
+                                margin="dense"
+                                name="spouse_name"
+                                label="שם בן הזוג"
+                                type="text"
+                                fullWidth
+                                value={donorDetails.spouse_name || ""}
+                                onChange={handleChange}
+                            />
+                            <TextField
+                                margin="dense"
+                                name="address_at_work"
+                                label="כתובת בעבודה"
+                                type="text"
+                                fullWidth
+                                value={donorDetails.address_at_work || ""}
+                                onChange={handleChange}
+                            />
+                            <TextField
+                                margin="dense"
+                                name="introduction_description"
+                                label="תיאור הכרות"
+                                type="text"
+                                fullWidth
+                                value={donorDetails.introduction_description || ""}
+                                onChange={handleChange}
+                            />
+                            <TextField
+                                margin="dense"
+                                name="remarks"
+                                label="הערות"
+                                type="text"
+                                fullWidth
+                                value={donorDetails.remarks || ""}
+                                onChange={handleChange}
+                            />
+                        </form>
+                    </DialogContent>
+                    <DialogActions>
+                        <Button onClick={handleClose} color="primary">ביטול</Button>
+                        <Button onClick={handleSubmit} color="primary">הוסף</Button>
+                    </DialogActions>
+                </Dialog>
+            </ThemeProvider>
+        </>
     );
 };
 
