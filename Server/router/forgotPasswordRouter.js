6a513029781b1fe4fd10a3765dae280d9f4f8ee3
מלכה נבנצל <Hand4c17@mbjcomp.org.il>
2024-07-05 14:05:50 +0300

    fix fogot password router

--- Server/router/forgotPasswordRouter.js
+++ Server/router/forgotPasswordRouter.js
@@ -2,7 +2,6 @@ import express from "express";
 import { ForgotPasswordController } from "../controllers/forgotPasswordController.js";
 
 const forgotPasswordRouter = express.Router();
-console.log("i in router")
 const forgotPasswordController = new ForgotPasswordController();
 forgotPasswordRouter.post("/:id", forgotPasswordController.forgotPassword)
 forgotPasswordRouter.put("/:id", forgotPasswordController.updateByOTP)
